461facf8a4231e4b9cc35fcf2f13d44c6fddab76
Yonatan Kof <kof@yonatankof.com>
2023-09-14 08:52:44 +0300

    Add nuxt/content & sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	modules: ['@nuxt/content'],
+	devtools: { enabled: true },
+	css: ['~/assets/styles/style.scss'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+						@use '~/assets/styles/reset' as *; 
+						@use '~/assets/styles/mixins' as *; 
+						@use '~/assets/styles/utopia' as *; 
+						@use '~/assets/styles/typography' as *;`,
+				},
+			},
+		},
+	},
+	app: {
+		pageTransition: {
+			name: 'page',
+			mode: 'out-in', // default
+		},
+		layoutTransition: {
+			name: 'layout',
+			mode: 'out-in', // default
+		},
+	},
+  runtimeConfig: {
+		// Private keys are only available on the server
+		// Public keys that are exposed to the client
+		public: {
+			apiSecret: process.env.NUXT_OMDBAPI_KEY,
+			siteOwnerName: 'Yonatan Ben Knaan',
+			missingImg: '/missing.svg',
+		},
+	},
+});
