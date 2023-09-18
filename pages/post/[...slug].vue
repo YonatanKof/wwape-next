f1570ed979ea16cc1695b8bff47d57d04c473107
Yonatan Kof <kof@yonatankof.com>
2023-09-19 00:28:48 +0300

    Add post

---
+++ pages/post/[...slug].vue
@@ -0,0 +1,12 @@
+<!-- This is a single design -->
+<template>
+	<main>
+		<ContentDoc v-slot="{ doc }">
+			<article>
+				<h1>{{ doc.title }}</h1>
+				<h4>Published on {{ doc.date }} by {{ doc.author_name }}</h4>
+				<ContentRenderer :value="doc" />
+			</article>
+		</ContentDoc>
+	</main>
+</template>
---
+++ pages/post/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+const config = useRuntimeConfig();
+useHead({
+	title: 'Posts',
+});
+useSeoMeta({
+	ogType: 'website',
+	description: () => `Amazing posts by ${config.public.siteOwnerName}`,
+	ogDescription: () => `Amazing posts by ${config.public.siteOwnerName}`,
+	twitterDescription: () => `Amazing posts by ${config.public.siteOwnerName}`,
+	image: '/images/posts.png',
+	ogImage: '/images/posts.png',
+	twitterImage: '/images/posts.png',
+	imageAlt: 'Social cover for this site posts page',
+	ogImageAlt: 'Social cover for this site posts page',
+	twitterImageAlt: 'Social cover for this site posts page',
+	twitterCard: 'summary_large_image',
+	twitterSite: '@yonatankof',
+});
+const { data } = await useAsyncData('equal', () => {
+	return queryContent('post').where({}).find();
+});
+</script>
+<template>
+	<div>
+		<h1>Posts</h1>
+		<ItemList :design-data="data" has-flex />
+	</div>
+</template>
