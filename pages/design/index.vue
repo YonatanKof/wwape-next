fe7e6e1e0cc368995cab81a77c4a66f9c62389c3
Yonatan Kof <kof@yonatankof.com>
2023-10-14 18:25:25 +0300

    Better SEO to design page

--- pages/design/index.vue
+++ pages/design/index.vue
@@ -1,35 +1,52 @@
-<template>
-    <main>
-        <section id="main" class="!pt-0">
-            <ContentQuery
-                path="/design"
-                :only="['title', 'date', '_path', 'cover_image']"
-                :sort="{
-                    date: -1
-                }"
-                :limit="designCountLimit"
-                v-slot="{ data }"
-            >
-                <ItemList :design-data="data" />
-            </ContentQuery>
-            <Pagination
-                v-if="data > 1"
-                class="mt-8"
-                :currentPage="1"
-                :totalPages="data"
-                :nextPage="data > 1"
-                baseUrl="/design"
-                pageUrl="/design/page/"
-            />
-        </section>
-    </main>
-</template>
-
 <script setup>
+const config = useRuntimeConfig();
+useHead({
+	title: 'Designs',
+});
+useSeoMeta({
+	ogType: 'website',
+	description: () => `Amazing designs by ${config.public.siteOwnerName}`,
+	ogDescription: () => `Amazing designs by ${config.public.siteOwnerName}`,
+	twitterDescription: () => `Amazing designs by ${config.public.siteOwnerName}`,
+	image: '/images/designs.png',
+	ogImage: '/images/designs.png',
+	twitterImage: '/images/designs.png',
+	imageAlt: 'Social cover for this site designs page',
+	ogImageAlt: 'Social cover for this site designs page',
+	twitterImageAlt: 'Social cover for this site designs page',
+	twitterCard: 'summary_large_image',
+	twitterSite: '@yonatankof',
+});
 // Find the number of blogs present
 const designCountLimit = 16;
 const { data } = await useAsyncData(`content-/design`, async () => {
-    const _designs = await queryContent('/design').only('title').find()
-    return Math.ceil(_designs.length / designCountLimit);
+	const _designs = await queryContent('/design').only('title').find();
+	return Math.ceil(_designs.length / designCountLimit);
 });
 </script>
+<template>
+	<main>
+		<section id="main" class="!pt-0">
+			<ContentQuery
+				path="/design"
+				:only="['title', 'date', '_path', 'cover_image']"
+				:sort="{
+					date: -1,
+				}"
+				:limit="designCountLimit"
+				v-slot="{ data }"
+			>
+				<ItemList :design-data="data" />
+			</ContentQuery>
+			<Pagination
+				v-if="data > 1"
+				class="mt-8"
+				:currentPage="1"
+				:totalPages="data"
+				:nextPage="data > 1"
+				baseUrl="/design"
+				pageUrl="/design/page/"
+			/>
+		</section>
+	</main>
+</template>
