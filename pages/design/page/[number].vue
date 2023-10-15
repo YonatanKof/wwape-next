019d30d8a021ccd8c2500557600b5d23bd6e209f
Yonatan Kof <kof@yonatankof.com>
2023-10-15 10:51:48 +0300

    Better SEO in design Nth page

--- pages/design/page/[number].vue
+++ pages/design/page/[number].vue
@@ -71,4 +71,23 @@ try {
     console.error(err);
     router.replace('/design/');
 }
+
+const config = useRuntimeConfig();
+useHead({
+	title: 'Designs',
+});
+useSeoMeta({
+	ogType: 'website',
+	description: () => `Amazing designs by ${config.public.siteOwnerName}`,
+	ogDescription: () => `Amazing designs by ${config.public.siteOwnerName}`,
+	twitterDescription: () => `Amazing designs by ${config.public.siteOwnerName}`,
+	image: '/images/designs.png',
+	ogImage: '/images/designs.png',
+	twitterImage: '/images/designs.png',
+	imageAlt: 'Social cover for this site designs page',
+	ogImageAlt: 'Social cover for this site designs page',
+	twitterImageAlt: 'Social cover for this site designs page',
+	twitterCard: 'summary_large_image',
+	twitterSite: '@yonatankof',
+});
 </script> 
