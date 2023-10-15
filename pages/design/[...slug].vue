2017b0d137b7829d07f9789c6f473cd1637861c5
Yonatan Kof <kof@yonatankof.com>
2023-10-15 10:51:04 +0300

    Better SEO in design post

--- pages/design/[...slug].vue
+++ pages/design/[...slug].vue
@@ -15,14 +15,19 @@ const baseUrl = 'https://wwape.com';
 const canonicalPath = baseUrl + (path + '/').replace(/\/+$/, '/');
 const image = data.value?.article?.social_image.src || '/images/main.png';
 
+const config = useRuntimeConfig();
+const siteTitle = `A design by ${config.public.siteOwnerName}`;
 
 useHead({
-    title: data.value?.article?.title,
+	titleTemplate: (titleChunk) => {
+		return titleChunk ? `${titleChunk} - ` + siteTitle : siteTitle;
+	},
+    // title: data.value?.article?.title,
     meta: [
         { name: 'description', content: data.value?.article?.description },
         { property: 'article:published_time', content: data.value?.article?.updated.split('T')[0] || data.value?.article?.date.split('T')[0] },
         // OG
-        { hid: 'og:title', property: 'og:title', content: data.value?.article?.title },
+        // { hid: 'og:title', property: 'og:title', content: data.value?.article?.title },
         { hid: 'og:url', property: 'og:url', content: canonicalPath },
         { hid: 'og:description', property: 'og:description', content: data.value?.article?.description },
         { hid: 'og:image', name: 'image', property: 'og:image', content: image },
@@ -33,7 +38,7 @@ useHead({
         { hid: 'og:image:alt', property: 'og:image:alt', content: data.value?.article?.social_image.alt },
         // Twitter
         { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-        { hid: 'twitter:title', name: 'twitter:title', content: data.value?.article?.title },
+        // { hid: 'twitter:title', name: 'twitter:title', content: data.value?.article?.title },
         { hid: 'twitter:url', name: 'twitter:url', content: canonicalPath },
         { hid: 'twitter:description', name: 'twitter:description', content: data.value?.article?.description },
         { hid: 'twitter:image', name: 'twitter:image', content: image },
