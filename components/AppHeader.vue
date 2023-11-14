35abad4bcc6af87241c590d17ba43dca637c0903
Yonatan Kof <kof@yonatankof.com>
2023-11-14 11:23:20 +0200

    Add drak toggle, reponsiveness and remove navbar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,18 +1,46 @@
 <script setup>
-const config = useRuntimeConfig();
+import { useDark, useToggle } from '@vueuse/core';
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 <template>
 	<header>
 		<h2>Yonatan <span>Ben Knaan</span></h2>
-		<Navbar />
+		<div>
+			<nav>
+				<nuxt-link :to="{ name: 'index' }">Home</nuxt-link>
+				<!-- <nuxt-link :to="{name: 'post'}">Posts</nuxt-link> -->
+				<nuxt-link :to="{ name: 'design' }">Designs</nuxt-link>
+			</nav>
+			<button @click="toggleDark()">
+				<span v-if="isDark">Light</span>
+				<span v-else>Dark</span>
+			</button>
+		</div>
 	</header>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
 header {
 	height: var(--header-height);
 	@include flex-center;
 }
 span {
 	white-space: nowrap;
+}
+div, nav {
+	@include flex-center;
+	gap: var(--space-xs);
+}
+@media screen and (max-width: 640px) {
+	header {
+		flex-direction: column-reverse;
+		align-items: start;
+		gap: var(--space-xs);
+		padding-block: var(--space-xs);
+		height: unset;
+	}
+	div{
+		width: 100%;
+	}
 }
 </style>
