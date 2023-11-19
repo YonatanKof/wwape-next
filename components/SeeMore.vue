52be6db057d43a286b29a0553494d141b8904fd0
Yonatan Kof <kof@yonatankof.com>
2023-11-19 16:34:30 +0200

    Better see more

--- components/SeeMore.vue
+++ components/SeeMore.vue
@@ -14,15 +14,22 @@ defineProps({
 <template>
 	<nav class="more">
 		<nuxt-link class="item" v-if="prev" :to="prev?._path">
-			<h4 id="" class="item-content"> Go back → {{ prev.title }}</h4>
+			<h4 id="" class="item-content">Go back → {{ prev.title }}</h4>
 			<img :src="prev?.social_image.src" :alt="prev?.social_image.alt" />
 		</nuxt-link>
-		<span v-else>The 1st item</span>
+		<nuxt-link v-else class="item">
+			<h4 id="" class="item-content">No previous items</h4>
+			<img src="/no-prev-next.webp" alt="An image for no previous items" />
+		</nuxt-link>
+		<!-- <span v-else>The 1st item</span> -->
 		<nuxt-link class="item" v-if="next" :to="next?._path">
-			<h4 id="" class="item-content"> Go next → {{ next.title }}</h4>
+			<h4 id="" class="item-content">Go next → {{ next.title }}</h4>
 			<img :src="next?.social_image.src" :alt="next?.social_image.alt" />
 		</nuxt-link>
-		<span v-else>The last item</span>
+		<nuxt-link v-else class="item">
+			<h4 id="" class="item-content">No next items</h4>
+			<img src="/no-prev-next.webp" alt="An image for no next items" />
+		</nuxt-link>
 	</nav>
 </template>
 
