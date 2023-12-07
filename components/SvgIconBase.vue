685a296de2301d9bee014f79393364857ea777b4
Yonatan Kof <kof@yonatankof.com>
2023-12-07 23:50:36 +0200

    Add icon system

---
+++ components/SvgIconBase.vue
@@ -0,0 +1,52 @@
+<script setup>
+defineProps({
+	viewBox: {
+		type: String,
+		default: '0 0 32 32',
+	},
+	size: {
+		type: String,
+		default: '1.25rem',
+		// default: 'var(--space-m)',
+	},
+	fillColor: {
+		type: String,
+		default: 'none',
+	},
+	strokeColor: {
+		type: String,
+		default: 'var(--color-sys-main)',
+	},
+	strokeWidth: {
+		type: String,
+		default: '0.125rem',
+		// default: 'var(--space-4xs)',
+	},
+});
+</script>
+
+<template>
+	<svg
+		version="1.1"
+		baseProfile="full"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		xmlns:ev="http://www.w3.org/2001/xml-events"
+		aria-labelledby="title description"
+		role="img"
+		:viewBox="viewBox"
+		:width="size"
+		:height="size"
+	>
+		<slot />
+		<rect id="bounding-box" fill="none" stroke="none" width="100%" height="100%" />
+	</svg>
+</template>
+
+<style>
+svg, path {
+	fill: v-bind(fillColor) !important;
+	stroke: v-bind(strokeColor);
+	stroke-width: v-bind(strokeWidth);
+}
+</style>
---
+++ components/icon/Auto.vue
@@ -0,0 +1,13 @@
+<template>
+	<title id="title">Auto icon</title>
+	<desc id="description">Icon for theme auto mood</desc>
+	<g fill-rule="evenodd">
+		<circle cx="16" cy="16" r="12"></circle>
+		<line x1="16" y1="4" x2="16" y2="28"></line>
+		<line x1="16" y1="16" x2="28" y2="16"></line>
+		<line x1="16" y1="12" x2="27" y2="12"></line>
+		<line x1="16" y1="20" x2="27" y2="20"></line>
+		<line x1="16" y1="8" x2="25" y2="8"></line>
+		<line x1="16" y1="24" x2="25" y2="24"></line>
+	</g>
+</template>
