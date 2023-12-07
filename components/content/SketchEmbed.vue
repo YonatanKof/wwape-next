80c0d60a8074e3f6aefc92542b04113e01a9923f
Yonatan Kof <kof@yonatankof.com>
2023-12-07 23:52:22 +0200

    Add SketchEmbed

---
+++ components/content/SketchEmbed.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+defineProps({
+	src: {
+		type: String,
+		required: true,
+	},
+	height: {
+		type: String,
+		default: 'var(--space-8xl)',
+	},
+});
+</script>
+
+<template>
+	<div class="frame">
+		<iframe class="iframe" :src="`https://www.sketch.com/embed/s/` + src" allowfullscreen> </iframe>
+	</div>
+</template>
+
+<style scoped>
+.frame {
+	left: 0;
+	width: 100%;
+	height: calc(v-bind(height) * 4);
+	position: relative;
+	margin-block: var(--space-m);
+}
+.iframe {
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	border: 0;
+	border-radius: var(--border-radius-regular);
+}
+</style>
