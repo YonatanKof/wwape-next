a754930675d7156c23b641d72ebb13a0648d2627
Yonatan Kof <kof@yonatankof.com>
2023-12-25 10:06:29 +0200

    Add ModalConfirmPlainCss and Preview components

---
+++ components/ModalConfirmPlainCss.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal';
+
+defineProps<{
+	title?: string;
+}>();
+
+const emit = defineEmits<{
+	(e: 'confirm'): void;
+}>();
+</script>
+
+<template>
+	<VueFinalModal
+		class="confirm-modal"
+		content-class="confirm-modal-content"
+		overlay-transition="vfm-fade"
+		content-transition="vfm-fade"
+	>
+		<h2>{{ title }}</h2>
+		<slot />
+		<button @click="emit('confirm')">Confirm</button>
+	</VueFinalModal>
+</template>
+
+<style lang="scss">
+.vfm--overlay{
+	background: var(--color-sys-slight);
+}
+.confirm-modal {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	// @include flex-center();
+}
+.confirm-modal-content {
+	display: flex;
+	flex-direction: column;
+	padding: var(--space-m);
+	background: var(--color-sys-invert);
+	border-radius: var(--border-radius-regular);
+	box-shadow: var(--shadow-lg);
+}
+.confirm-modal-content > * + * {
+}
+.confirm-modal-content h1 {
+}
+.confirm-modal-content button {
+}
+</style>
---
+++ components/Preview.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ModalsContainer, useModal } from 'vue-final-modal';
+import ModalConfirmPlainCss from '~/components/ModalConfirmPlainCss.vue';
+
+const { open, close } = useModal({
+	component: ModalConfirmPlainCss,
+	attrs: {
+		title: 'Hello World!',
+		onConfirm() {
+			close();
+		},
+	},
+	slots: {
+		default: '<p>The content of the modal</p>',
+	},
+});
+</script>
+
+<template>
+	<div><button @click="open">Open Modal</button> <ModalsContainer /></div>
+</template>
