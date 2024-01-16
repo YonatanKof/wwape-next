a1fadbc6dd0732d9fa09f14c0d4005403121332b
Yonatan Kof <kof@yonatankof.com>
2024-01-16 11:15:17 +0200

    Add SayHi component for displaying random greetings

---
+++ components/SayHi.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { rand } from '@vueuse/shared';
+
+const greetings = ['שלום', 'Hi', 'Yo!', 'Hey', 'Hola', 'こんにちは', 'Bonjour', 'Salut', '你好', 'Привет'];
+const word = ref('Hello');
+
+function runIt() {
+	word.value = greetings[rand(0, greetings.length - 1)];
+}
+
+setInterval(runIt, 1250);
+</script>
+
+<template>
+	<span>{{ word }}</span>
+</template>
