ea086dc44520e9b9457ec38c52b147155dddc1d6
Yonatan Kof <kof@yonatankof.com>
2024-01-16 12:13:22 +0200

    Update SayHi component to use VueTypewriterEffect

--- components/SayHi.vue
+++ components/SayHi.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { rand } from '@vueuse/shared';
+import VueTypewriterEffect from "vue-typewriter-effect";
+// import { rand } from '@vueuse/shared';
 
-const greetings = ['שלום', 'Hi', 'Yo!', 'Hey', 'Hola', 'こんにちは', 'Bonjour', 'Salut', '你好', 'Привет'];
-const word = ref('Hello');
+// const greetings = ['שלום', 'Hi', 'Yo!', 'Hey', 'Hola', 'こんにちは', 'Bonjour', 'Salut', '你好', 'Привет'];
+// const word = ref('Hello');
 
-function runIt() {
-	word.value = greetings[rand(0, greetings.length - 1)];
-}
+// function runIt() {
+// 	word.value = greetings[rand(0, greetings.length - 1)];
+// }
 
-setInterval(runIt, 1250);
+// setInterval(runIt, 1250);
 </script>
 
 <template>
-	<span>{{ word }}</span>
+	<!-- <span>{{ word }}</span> -->
+	<vue-typewriter-effect :strings="['Hello', 'שלום', 'Привіт', 'Hola', 'こんにちは', 'Hey', 'Bonjour', 'Salut', '你好', 'Привет']" />
 </template>
