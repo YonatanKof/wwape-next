79f76c15c75aebe544965bc8387e429b224a49d1
Yonatan Kof <kof@yonatankof.com>
2024-01-16 11:16:21 +0200

    Refactor IntroText component to include new props and styling

--- components/IntroText.vue
+++ components/IntroText.vue
@@ -9,20 +9,35 @@ const props = defineProps({
 	text2: {
 		type: String,
 	},
-}); 
+	isOn: {
+		type: Boolean,
+		default: false,
+	},
+});
 </script>
 <template>
 	<div>
 		<h1 v-if="title">{{ title }}</h1>
-		<p v-if="text">{{ text }}</p>
-		<p v-if="text2">{{ text2 }}</p>
+		<span
+			><p v-if="text">
+				<i><SayHi v-if="isOn" /></i>{{ text }}
+			</p>
+			<p v-if="text2" id="last">{{ text2 }}</p></span
+		>
 	</div>
 </template>
 <style scoped>
 p {
 	font-size: var(--step-2);
+	max-width: 55ch;
+}
+#last {
+	margin-block-start: var(--space-2xs);
 }
 div {
 	margin-block-end: var(--space-m);
+}
+i {
+	font-weight: 500;
 }
 </style>
