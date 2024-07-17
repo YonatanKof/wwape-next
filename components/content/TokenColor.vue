006ab48544fb677cf15bfb09516f878b98f3261c
Yonatan Kof <kof@yonatankof.com>
2024-07-17 09:35:27 +0300

    feat: Add TokenColor component to update CSS variables
    Add TokenColor component to allow the user to choose a color and update
    corresponding CSS variables.

---
+++ components/content/TokenColor.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const color = ref('hsl(180, 25%, 15%)'); // Initial color value
+
+const updateColorVariable = (newColor) => {
+	document.documentElement.style.setProperty('--color-sys-main', newColor);
+};
+
+watch(color, (newColor) => {
+	updateColorVariable(newColor);
+});
+
+onMounted(() => {
+	updateColorVariable(color.value); // Set initial color value on mount
+});
+</script>
+<template>
+	<div>
+		<label for="colorInput">Choose a color:</label>
+		<input type="color" id="colorInput" v-model="color"/>
+		<div>
+			<span style="background-color: var(--color-sys-main)" />
+			<span style="background-color: var(--color-sys-slight)" />
+			<span style="background-color: var(--color-sys-dim)" />
+			<span style="background-color: var(--color-sys-dis)" />
+			<span style="background-color: var(--color-sys-none)" />
+		</div>
+	</div>
+</template>
+<style scoped>
+span {
+	display: inline-block;
+	width: var(--space-xl);
+	height: var(--space-xl);
+	border-radius: var(--border-radius-md);
+}
+</style>
