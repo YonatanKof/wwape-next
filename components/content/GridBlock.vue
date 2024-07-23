db8fb273a5738631fcea6dd7c7cca474444304f6
Yonatan Kof <kof@yonatankof.com>
2024-07-23 16:31:56 +0300

    chore: Update GridBlock and GridUnit components

--- components/content/GridBlock.vue
+++ components/content/GridBlock.vue
@@ -1,29 +1,33 @@
 <script setup>
 defineProps({
 	colNum: {
-		type: Number,
-		default: 1,
+		type: String,
+		default: '1',
 	},
 	rowNum: {
-		type: Number,
-		default: 1,
+		type: String,
+		default: '1',
+	},
+	gap: {
+		type: String,
+		default: 'var(--space-xs)',
 	},
 });
 </script>
 <template>
 	<div>
-        <slot />
-    </div>
+		<slot />
+	</div>
 </template>
 <style lang="scss" scoped>
 div {
 	display: grid;
 	grid-template-columns: repeat(v-bind(colNum), 1fr);
 	grid-template-rows: repeat(v-bind(rowNum), 1fr);
-	margin-block: var(--space-s);
-	gap: var(--space-xs);
+	gap: v-bind(gap);
 	height: 100%;
 	width: 100%;
+	margin-block: var(--space-xs);
 	@media (width <= $display-width-xs) {
 		grid-template-columns: 1fr;
 	}
---
+++ components/content/GridUnit.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+	marginBlock: {
+		type: String,
+		default: undefined,
+	},
+});
+</script>
+
+<template>
+	<div>
+		<slot />
+	</div>
+</template>
+<style>
+</style>
+<style scoped>
+div {
+	margin-block: v-bind(marginBlock);
+}
+</style>
