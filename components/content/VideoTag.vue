8eb22a077b211fa6817ef89d23c07c9ce1ac864d
Yonatan Kof <kof@yonatankof.com>
2024-07-23 16:32:40 +0300

    style: Add showMesh prop and description to VideoTag component

--- components/content/VideoTag.vue
+++ components/content/VideoTag.vue
@@ -24,20 +24,50 @@ defineProps({
 		type: Boolean,
 		default: true,
 	},
+	showMesh: {
+		type: Boolean,
+		default: false,
+	},
+	desc: {
+		type: String,
+		default: '',
+	},
 });
 </script>
 <template>
-	<video :controls="controls" :autoplay="autoplay" :muted="muted" :loop="loop">
-		<source :src="src" type="video/webm" />
-		<p>Your browser does not support <code>video</code> tag</p>
-	</video>
+	<div>
+		<span v-if="showMesh"></span>
+		<video :controls="controls" :autoplay="autoplay" :muted="muted" :loop="loop">
+			<source :src="src" type="video/webm" />
+			<p>Your browser does not support <code>video</code> tag</p>
+		</video>
+		<em v-if="desc">{{ desc }}</em>
+	</div>
 </template>
-<style scoped>
-video {
+<style lang="scss" scoped>
+div {
 	margin-block: var(--space-xs);
+	display: block;
+	width: 100%;
+	height: min-content;
+	position: relative;
+}
+span {
+	border-radius: var(--border-radius-xs);
+	position: absolute;
+	clear: both;
+	background-image: radial-gradient(transparent 1px, var(--color-link-dim) 1px);
+	background-size: 2px 2px;
+	inset: 0;
+}
+video {
 	max-width: 100%;
 	aspect-ratio: v-bind(aspectRatio);
 	border-radius: var(--border-radius-xs);
 	overflow: hidden;
+	display: flow-root;
+}
+em {
+	@include desc-text-under();
 }
 </style>
