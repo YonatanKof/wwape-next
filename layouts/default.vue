cafb165ac0cf47b2a529319da4e1e07e68a09969
Yonatan Kof <kof@yonatankof.com>
2024-07-24 10:12:15 +0300

    feat: Add simple layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ModalsContainer } from 'vue-final-modal';
+</script>
+<template>
+	<main class="main-app">
+		<AppHeader />
+		<NuxtPage />
+		<ModalsContainer />
+		<AppFooter />
+	</main>
+</template>
+<style lang="scss">
+
+.main-app {
+	height: 100dvh;
+	display: grid;
+	grid-template-rows: auto 1fr auto;
+	@include display-width;
+}
+</style>
---
+++ layouts/simple.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+<template>
+	<main class="simple-warp">
+		<!-- <main class="main-app-simple"> -->
+		<main class="simple-app">
+			<AppHeader />
+			<NuxtPage />
+		</main>
+	</main>
+</template>
+<style lang="scss">
+.simple-warp {
+	display: flex;
+	justify-content: center; /* Horizontal */
+	align-items: center; /* Vertical */
+	min-height: 100vh;
+	background: radial-gradient(ellipse at top left, var(--color-sys-main), transparent 50%),
+		radial-gradient(ellipse at bottom right, var(--color-link-main), transparent 50%);
+	// height: calc(100vh - calc(var(--space-xl) * 2));
+}
+.main-app-simple {
+	// height: 100%;
+	// max-height: calc(100vh - calc(var(--space-xl) * 4));
+	// height: 90vh;
+	// overflow: hidden;
+	// margin: var(--space-xl);
+	padding: var(--space-xl);
+	background-color: var(--color-sys-invert-highlight);
+	max-width: var(--display-width-lg);
+	border-radius: var(--border-radius-md);
+}
+.simple-app {
+	max-width: var(--display-width-lg);
+	margin-inline: var(--space-m);
+}
+</style>
