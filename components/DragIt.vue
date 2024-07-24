cbc9aea16125e1d278e922a37d01d0f82859f9c6
Yonatan Kof <kof@yonatankof.com>
2024-07-24 14:19:33 +0300

    feat: Add DragIt component for draggable elements

---
+++ components/DragIt.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+	xPos: {
+		type: String,
+		default: 'calc(50% - 10rem);',
+	},
+	yPos: {
+		type: String,
+		default: 'calc(100% - 12rem);',
+	},
+	element: {
+		type: String,
+		default: null,
+	},
+});
+
+import { useDraggable } from '@vueuse/core';
+const el = ref(null);
+// const el = ref(props.element);
+const { x, y, style } = useDraggable(el, {
+	initialValue: { x: props.xPos, y: props.yPos },
+});
+</script>
+
+<template>
+	<div ref="el" :style="style">
+		<slot />
+	</div>
+</template>
