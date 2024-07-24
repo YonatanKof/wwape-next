2372a38a7546afccd74c840614caa1cc50a5fcb8
Yonatan Kof <kof@yonatankof.com>
2024-07-24 14:26:50 +0300

    refactor: Update simple layout with background gradient and margin adjustments

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts"></script>
 <template>
 	<main class="simple-warp">
-		<!-- <main class="main-app-simple"> -->
 		<main class="simple-app">
 			<AppHeader />
 			<NuxtPage />
@@ -14,23 +13,11 @@
 	justify-content: center; /* Horizontal */
 	align-items: center; /* Vertical */
 	min-height: 100vh;
-	background: radial-gradient(ellipse at top left, var(--color-sys-main), transparent 50%),
-		radial-gradient(ellipse at bottom right, var(--color-link-main), transparent 50%);
-	// height: calc(100vh - calc(var(--space-xl) * 2));
-}
-.main-app-simple {
-	// height: 100%;
-	// max-height: calc(100vh - calc(var(--space-xl) * 4));
-	// height: 90vh;
-	// overflow: hidden;
-	// margin: var(--space-xl);
-	padding: var(--space-xl);
-	background-color: var(--color-sys-invert-highlight);
-	max-width: var(--display-width-lg);
-	border-radius: var(--border-radius-md);
+	background: radial-gradient(ellipse at bottom right, var(--color-link-dim), transparent 50%);
 }
 .simple-app {
 	max-width: var(--display-width-lg);
 	margin-inline: var(--space-m);
+	min-height: 100dvh;
 }
 </style>
