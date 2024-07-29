4550323625f0756a314a4ba9dc3d07a6addd03b2
Yonatan Kof <kof@yonatankof.com>
2024-07-29 18:52:34 +0300

    feat: Update CV data for Pepperi and Bit.dev experiences

--- components/KofCv.vue
+++ components/KofCv.vue
@@ -23,22 +23,26 @@ const infoObj = {
 	creationDate: new Date(),
 };
 
-let textColor = 'hsl(180, 25%, 15%)';
-let brandColor = 'hsl(280, 100%, 35%)';
 let pdfConfig = {
 	fontRegister: {
 		ShantellSans: '/misc/ShantellSans-MediumItalic.ttf',
 		Inter: '/misc/Inter-Regular.otf',
 	},
 };
+
+let textColor = 'hsl(180, 25%, 15%)';
+let brandColor = 'hsla(280, 100%, 50%)';
+let expPepperiY = 131;
+let expBitY = 370;
+let expMtnsY = 480;
 let firstCol = 54;
 let firstColWidth = 320;
-let expPepperiY = 131;
 let bodyTextLg = '8.5px Inter';
 let bodyText = '7.25px Inter';
 let h1Text = '13px ShantellSans';
 let h2Text = '9px ShantellSans';
-let h3Text = '8.25px ShantellSans';
+let h3Text = '7.5px ShantellSans';
+let bulletSize = 1.9;
 </script>
 
 <template>
@@ -59,46 +63,46 @@ let h3Text = '8.25px ShantellSans';
 			:y="29"
 			:text="config.public.siteOwnerName"
 			:width="firstColWidth"
-			:style="{ fill: textColor, font: h1Text, align: 'left' }"
+			:style="{ fill: textColor, font: h1Text, align: 'left', lineGap: 3 }"
 		/>
 		<i-text
 			:x="firstCol"
 			:y="49"
 			:text="dataCv.textIntro"
 			:width="firstColWidth"
-			:style="{ fill: textColor, font: bodyTextLg, align: 'left' }"
+			:style="{ fill: textColor, font: bodyTextLg, align: 'left', lineGap: 3 }"
 		/>
 		<i-text
 			:x="firstCol"
 			:y="109"
 			text="Experience"
 			:width="firstColWidth"
-			:style="{ fill: textColor, font: h1Text, align: 'left' }"
+			:style="{ fill: textColor, font: h1Text, align: 'left', lineGap: 3 }"
 		/>
 		<i-group :x="expPepperiY" id="exp-pepperi">
 			<i-text
 				:x="firstCol"
 				:y="expPepperiY"
-				:text="dataCv.experience.date + ', ' + dataCv.experience.role + ': ' + dataCv.experience.name"
+				:text="dataCv.Pepperi.date + ', ' + dataCv.Pepperi.role + ': ' + dataCv.Pepperi.name"
 				:width="firstColWidth"
 				:style="{ fill: textColor, font: h2Text }"
 			/>
 			<i-text
 				:x="firstCol"
 				:y="expPepperiY + 19"
-				:text="dataCv.experience.intro"
+				:text="dataCv.Pepperi.intro"
 				:width="firstColWidth"
-				:style="{ fill: textColor, font: bodyText, align: 'left' }"
+				:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
 			/>
-			<i-group v-for="bullet in dataCv.experience.bullets" :key="bullet">
+			<i-group v-for="bullet in dataCv.Pepperi.bullets" :key="bullet">
 				<i-text
-				:x="firstCol + 10"
-				:y="(expPepperiY += bullet.number)"
-				:width="firstColWidth - 10"
-				:text="bullet.text"
-				:style="{ fill: textColor, font: bodyText, align: 'left' }"
+					:x="firstCol + 10"
+					:y="(expPepperiY += bullet.number)"
+					:width="firstColWidth - 10"
+					:text="bullet.text"
+					:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
 				/>
-				<i-circle :cx="firstCol+4" :cy="expPepperiY + 4" :r="1.8" :style="{ fillStyle: brandColor }" />
+				<i-circle :cx="firstCol + 4" :cy="expPepperiY + 4" :r="bulletSize" :style="{ fillStyle: brandColor }" />
 			</i-group>
 			<i-text
 				:x="firstCol"
@@ -107,24 +111,67 @@ let h3Text = '8.25px ShantellSans';
 				:width="firstColWidth"
 				:style="{ fill: textColor, font: h3Text }"
 			/>
-			<!-- <i-text
-				v-for="achievement in dataCv.experience.achievements"
-				:key="achievement"
-				:x="firstCol"
-				:y="(expPepperiY += achievement.number)"
-				:text="achievement.text"
-				:width="firstColWidth"
-				:style="{ fill: textColor, font: bodyText, align: 'left' }"
-			/> -->
-			<i-group v-for="achievement in dataCv.experience.achievements" :key="achievement">
+			<i-group v-for="achievement in dataCv.Pepperi.achievements" :key="achievement">
 				<i-text
-				:x="firstCol + 10"
-				:y="(expPepperiY += achievement.number)"
-				:width="firstColWidth - 10"
-				:text="achievement.text"
-				:style="{ fill: textColor, font: bodyText, align: 'left' }"
+					:x="firstCol + 10"
+					:y="(expPepperiY += achievement.number)"
+					:width="firstColWidth - 10"
+					:text="achievement.text"
+					:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
 				/>
-				<i-circle :cx="firstCol+4" :cy="expPepperiY + 4" :r="1.8" :style="{ fillStyle: brandColor }" />
+				<i-circle :cx="firstCol + 4" :cy="expPepperiY + 4" :r="bulletSize" :style="{ fillStyle: brandColor }" />
+			</i-group>
+		</i-group>
+		<i-group :x="expBitY" id="exp-bit">
+			<i-text
+				:x="firstCol"
+				:y="expBitY"
+				:text="dataCv.Bit.date + ', ' + dataCv.Bit.role + ': ' + dataCv.Bit.name"
+				:width="firstColWidth"
+				:style="{ fill: textColor, font: h2Text }"
+			/>
+			<i-text
+				:x="firstCol"
+				:y="expBitY + 19"
+				:text="dataCv.Bit.intro"
+				:width="firstColWidth"
+				:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
+			/>
+			<i-group v-for="bullet in dataCv.Bit.bullets" :key="bullet">
+				<i-text
+					:x="firstCol + 10"
+					:y="(expBitY += bullet.number)"
+					:width="firstColWidth - 10"
+					:text="bullet.text"
+					:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
+				/>
+				<i-circle :cx="firstCol + 4" :cy="expBitY + 4" :r="bulletSize" :style="{ fillStyle: brandColor }" />
+			</i-group>
+		</i-group>
+		<i-group :x="expMtnsY" id="exp-bit">
+			<i-text
+				:x="firstCol"
+				:y="expMtnsY"
+				:text="dataCv.MTNS.date + ', ' + dataCv.MTNS.role + ': ' + dataCv.MTNS.name"
+				:width="firstColWidth"
+				:style="{ fill: textColor, font: h2Text }"
+			/>
+			<i-text
+				:x="firstCol"
+				:y="expMtnsY + 19"
+				:text="dataCv.MTNS.intro"
+				:width="firstColWidth"
+				:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
+			/>
+			<i-group v-for="bullet in dataCv.MTNS.bullets" :key="bullet">
+				<i-text
+					:x="firstCol + 10"
+					:y="(expMtnsY += bullet.number)"
+					:width="firstColWidth - 10"
+					:text="bullet.text"
+					:style="{ fill: textColor, font: bodyText, align: 'left', lineGap: 3 }"
+				/>
+				<i-circle :cx="firstCol + 4" :cy="expMtnsY + 4" :r="bulletSize" :style="{ fillStyle: brandColor }" />
 			</i-group>
 		</i-group>
 	</pdfFrame>
