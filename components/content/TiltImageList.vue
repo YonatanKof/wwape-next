255cc52f9f9e7fb687c54be809b114066ce09309
Yonatan Kof <kof@yonatankof.com>
2024-08-04 15:36:37 +0300

    feat: Mage titles image

--- components/content/TiltImageList.vue
+++ components/content/TiltImageList.vue
@@ -3,8 +3,7 @@ import PepperiImages from '~/assets/data/PepperiImages.json';
 </script>
 <template>
 	<div>
-		<NuxtImg v-for="(image, index) in PepperiImages"  :class="`img${index}`" :src="image.src" />
-		<!-- <pre v-for="(image, index) in PepperiImages" >{{image.src}}</pre> -->
+		<NuxtImg v-for="(image, index) in PepperiImages" :class="`img${index}`" :src="image.src" />
 	</div>
 </template>
 <style scoped>
@@ -21,73 +20,64 @@ div {
 	display: grid;
 	grid-template-columns: repeat(12, 1fr);
 	grid-template-rows: repeat(auto, auto);
-	transform: translateX(3vh) rotate3d(4, 3, -2, 15deg);
+	transform-style: preserve-3d;
+	transform: translateX(3vh) rotate3d(6, 3, -2, 56deg);
 	max-width: var(--display-width-sm);
 	width: 100%;
 	margin-inline: auto;
+	transition: transform 0.5s ease-in-out;
+	&:hover {
+		transform: translateX(3vh) rotate3d(6, 3, -2, 0deg);
+	}
 }
 img {
 	margin: unset;
 	border-radius: var(--border-radius-xs);
-	filter: drop-shadow(0 var(--space-s) var(--space-s) var(--color-shadow));
+	filter: drop-shadow(0 var(--space-s) var(--space-xs) var(--color-black-dim));
 }
 .img0 {
-	animation: perspective 5s ease-in-out alternate infinite;
-	grid-column: 2 / span 7;
-	grid-row: 1 / span 4;
+	grid-column: 5 / span 6;
+	grid-row: 1 / span 3;
+	transform: translateZ(-1rem);
 }
 .img1 {
-	animation: perspective 3s ease-in-out alternate infinite;
-	transition-delay: 125ms;
-	grid-column: 6 / span 7;
-	grid-row: 2 / span 4;
-	z-index: 1;
+	grid-column: 1 / span 7;
+	grid-row: 2 / span 3;
+	transform: translateZ(1rem);
 }
 .img2 {
-	animation: perspective 4s ease-in-out alternate infinite;
-	transition-delay: 250ms;
-	grid-column: 3 / span 7;
-	grid-row: 4 / span 4;
-	z-index: 2;
+	grid-column: 6 / span 6;
+	grid-row: 3 / span 3;
+	transform: translateX(6.5rem);
 }
 .img3 {
-	animation: perspective 5s ease-in-out alternate infinite;
-	grid-column: 6 / span 7;
-	grid-row: 6 / span 4;
-	z-index: 3;
+	grid-column: 2 / span 6;
+	grid-row: 4 / span 3;
+	transform: translateZ(1.5rem);
 }
 .img4 {
-	animation: perspective 3s ease-in-out alternate infinite;
-	transition-delay: 125ms;
-	grid-column: 1 / span 7;
-	grid-row: 7 / span 4;
-	z-index: 4;
+	grid-column: 6 / span 7;
+	grid-row: 5 / span 3;
+	transform: translateZ(2.5rem);
 }
 .img5 {
-	animation: perspective 4s ease-in-out alternate infinite;
-	transition-delay: 250ms;
-	grid-column: 3 / span 7;
-	grid-row: 8 / span 4;
-	z-index: 5;
+	grid-column: 1 / span 8;
+	grid-row: 6 / span 3;
+	transform: translateX(-8rem) translateZ(-2rem);
 }
 .img6 {
-	animation: perspective 5s ease-in-out alternate infinite;
-	grid-column: 1 / span 7;
-	grid-row: 9 / span 4;
-	z-index: 6;
+	grid-column: 6 / span 6;
+	grid-row: 7 / span 3;
+	transform: translateX(-1rem) translateY(-1rem);
 }
 .img7 {
-	animation: perspective 3s ease-in-out alternate infinite;
-	transition-delay: 125ms;
-	grid-column: 6 / span 7;
-	grid-row: 10 / span 4;
-	z-index: 7;
+	grid-column: 2 / span 6;
+	grid-row: 8 / span 3;
+	transform: translateZ(-7rem);
 }
 .img8 {
-	animation: perspective 4s ease-in-out alternate infinite;
-	transition-delay: 250ms;
-	grid-column: 3 / span 7;
-	grid-row: 11 / span 4;
-	z-index: 8;
+	grid-column: 4 / span 7;
+	grid-row: 9 / span 3;
+	transform: translateZ(1.5rem) translateY(-1.5rem);
 }
 </style>
