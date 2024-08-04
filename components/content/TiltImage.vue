b0e799a342612c5e9bf769e0f7a073a43a9daf61
Yonatan Kof <kof@yonatankof.com>
2024-08-04 15:37:35 +0300

    improve: Update TiltImage component to include image descriptions

--- components/content/TiltImage.vue
+++ components/content/TiltImage.vue
@@ -12,43 +12,58 @@ const props = defineProps({
 		type: String,
 		default: undefined,
 	},
+	desc: {
+		type: String,
+		default: undefined,
+	},
 });
 </script>
 <template>
-	<div>
-		<NuxtImg vi-if="img1" class="img1" :src="img1" />
-		<NuxtImg vi-if="img2" class="img2" :src="img2" />
-		<NuxtImg vi-if="img3" class="img3" :src="img3" />
-	</div>
+	<span>
+		<div>
+			<NuxtImg vi-if="img1" class="img1" :src="img1" />
+			<NuxtImg vi-if="img2" class="img2" :src="img2" />
+			<NuxtImg vi-if="img3" class="img3" :src="img3" />
+		</div>
+		<em v-if="desc" >{{ desc }}</em>
+	</span>
 </template>
-<style scoped>
-@keyframes perspective {
-	from {
-		transform: translate(1px, 1px);
-	}
-
-	to {
-		transform: translate(-2px, -2px);
-	}
+<style lang="scss" scoped>
+em {
+	@include desc-text-under();
+	margin-block-start: var(--space-2xl);
+	margin-block-end: unset;
+}
+span {
+	display: block;
+	background-color: var(--color-sys-dis);
+	border-radius: var(--border-radius-sm);
+	padding: var(--space-l);
 }
 div {
 	display: grid;
 	grid-template-columns: repeat(12, 1fr);
 	grid-template-rows: repeat(7, 1fr);
-	transform: translateX(3vh) perspective(40rem) rotate3d(4, 3, -2, 15deg);
+	transform: rotate3d(4, 3, -2, 24deg);
 	max-width: var(--display-width-sm);
 	width: 100%;
 	margin-inline: auto;
+	transition: transform 0.5s ease-in-out;
+	transform-style: preserve-3d;
+	&:hover {
+		transform: rotate3d(4, 3, -2, 0deg) translateZ(0);
+	}
 }
 img {
 	margin: unset;
 	border-radius: var(--border-radius-xs);
-	filter: drop-shadow(0 var(--space-s) var(--space-s) var(--color-shadow));
+	filter: drop-shadow(0 var(--space-s) var(--space-s) var(--color-black-dim));
 }
 .img1 {
 	animation: perspective 5s ease-in-out alternate infinite;
 	grid-column: 1 / span 7;
 	grid-row: 1 / span 4;
+	transform: translateZ(-1rem);
 }
 .img2 {
 	animation: perspective 3s ease-in-out alternate infinite;
@@ -56,6 +71,7 @@ img {
 	grid-column: 6 / span 7;
 	grid-row: 2 / span 4;
 	z-index: 1;
+	transform: translateZ(2rem);
 }
 .img3 {
 	animation: perspective 4s ease-in-out alternate infinite;
@@ -63,5 +79,6 @@ img {
 	grid-column: 3 / span 7;
 	grid-row: 4 / span 4;
 	z-index: 0;
+	transform: translateZ(0.5rem);
 }
 </style>
