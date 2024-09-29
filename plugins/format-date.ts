aa0eb230d25286ee3096d15e6a954d076a98f27d
Yonatan Kof <kof@yonatankof.com>
2024-09-29 15:57:25 +0300

    improve: Update formatDate function to accept string, number, or Date

--- plugins/format-date.ts
+++ plugins/format-date.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(() => {
     return {
         provide: {
-            formatDate(date) {
+            formatDate(date: string | number | Date) {
                 // const options = { year: 'numeric', month: 'short', day: 'numeric' };
                 const options = { year: 'numeric', month: 'short' };
                 return new Date(date).toLocaleDateString('en', options);
