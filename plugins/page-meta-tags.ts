6e8eeab97e2ffc35c83bf2757a3b891a5243f862
Yonatan Kof <kof@yonatankof.com>
2024-09-29 16:16:07 +0300

    improve: Update page-meta-tags plugin to use reactive tags and improve code structure

--- plugins/page-meta-tags.ts
+++ plugins/page-meta-tags.ts
@@ -1,13 +1,21 @@
-// const config = useRuntimeConfig();
 export default defineNuxtPlugin(() => {
 	const config = useRuntimeConfig();
+	const { path } = useRoute();
+	const cleanPath = path.replace(/\/+$/, '');
+	const dynamicUrl = config.public.baseUrl;
+	const canonicalPath = dynamicUrl + cleanPath;
+
 	// This is my abstraction for setting meta tags
+	// const usePageHeadTag = () => {
+	// 	console.log('This is another function being used in the plugin.');
+	// };
 	const usePageMetaTags = ({
-		pageTitle = `A site by ${config.public.siteOwnerName} of the ${config.public.siteName}`,
-		pageDesc = `Explore creative graphic design, illustration, and UX/UI projects. Dive into blog articles on branding, design systems, and design, or enjoy curated DJ mixes and playlists`,
-		pageImg = '/images/index-social.jpg',
-		pageImgAlt = 'Yonatan Kof',
-		pageOgType = 'website' as
+		metaTitle = `A site by ${config.public.siteOwnerName} of the ${config.public.siteName}`,
+		metaTitleData = undefined,
+		metaDesc = `Explore creative graphic design, illustration, and UX/UI projects. Dive into blog articles on branding, design systems, and design, or enjoy curated DJ mixes and playlists`,
+		metaImg = '/images/index-social.jpg',
+		metaImgAlt = 'Social cover image for this site',
+		metaOgType = 'website' as
 			| 'website'
 			| 'article'
 			| 'book'
@@ -22,25 +30,49 @@ export default defineNuxtPlugin(() => {
 			| 'video.other'
 			| null
 			| undefined,
-		pageTwitterCard = 'summary_large_image' as 'summary_large_image' | 'summary' | 'app' | 'player' | null | undefined,
-		pageTwitterSite = '@yonatankof',
+		metaTwitterCard = 'summary_large_image' as 'summary_large_image' | 'summary' | 'app' | 'player' | null | undefined,
+		metaTwitterSite = '@yonatankof',
+		metaImageWidth = 1200,
+		metaImageHeight = 630,
+		metaImageType = 'image/jpeg' as 'image/jpeg' | 'image/gif' | 'image/png' | null | undefined,
+		metaCanonicalPath = canonicalPath,
+		metaPublishedTime = undefined,
+		metaModifiedTime = undefined,
 	} = {}) => {
+		useHead({
+			titleTemplate: (titleChunk) => {
+				return titleChunk ? `${titleChunk} - ` + metaTitle : metaTitle;
+			},
+			link: [
+				{
+					hid: 'canonical',
+					rel: 'canonical',
+					href: metaCanonicalPath,
+				},
+			],
+		});
 		// This is Nuxt 3's way of setting meta tags
 		// https://nuxt.com/docs/api/composables/use-seo-meta
 		// For reactive tags, use computed getter syntax: `key: () => value,`
-		// For example `title: () => pageTitle,`
-		return useSeoMeta({
-			title: () => pageTitle,
-			description: () => pageDesc,
-			ogDescription: () => pageDesc,
-			twitterDescription: () => pageDesc,
-			ogType: pageOgType,
-			ogImage: pageImg,
-			ogImageAlt: pageImgAlt,
-			twitterCard: pageTwitterCard,
-			twitterSite: pageTwitterSite,
-			twitterImage: pageImg,
-			twitterImageAlt: pageImgAlt,
+		// For example `description: () => metaDesc`
+		useSeoMeta({
+			description: () => metaDesc,
+			articlePublishedTime: metaPublishedTime,
+			articleModifiedTime: metaModifiedTime,
+			ogDescription: () => metaDesc,
+			twitterDescription: () => metaDesc,
+			ogTitle: metaTitleData ? metaTitleData + ` - ` + metaTitle : metaTitle,
+			ogType: metaOgType,
+			ogImage: metaImg,
+			ogImageAlt: metaImgAlt,
+			ogImageWidth: metaImageWidth,
+			ogImageHeight: metaImageHeight,
+			ogImageType: metaImageType,
+			twitterTitle: metaTitleData ? metaTitleData + ` - ` + metaTitle : metaTitle,
+			twitterCard: metaTwitterCard,
+			twitterSite: metaTwitterSite,
+			twitterImage: metaImg,
+			twitterImageAlt: metaImgAlt,
 		});
 	};
 
