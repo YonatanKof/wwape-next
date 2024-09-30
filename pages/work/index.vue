36b87ba3d3336f6304e2c6c3690dc3145434b7cf
Yonatan Kof <kof@yonatankof.com>
2024-09-30 19:39:24 +0300

    feat: Add work page (now just placeholder)

---
+++ pages/work/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+const config = useRuntimeConfig();
+
+const nuxtApp = useNuxtApp();
+nuxtApp.$pageMetaTags({
+	metaTitle: `Amazing product works by ${config.public.siteOwnerName} of the ${config.public.siteName}`,
+	metaDesc: 'Browse a collection of professional design projects ...',
+	metaImg: '/images/posts-social.jpg',
+	metaImgAlt: 'Social cover for this site work page',
+});
+const { data } = await useAsyncData('equal', () => {
+	return queryContent('work').sort({ date: -1 }).where({}).find();
+});
+</script>
+<template>
+	<div>
+		<IntroText title="Work" text="Welcome to my work page..."/>
+		<ItemList :design-data="data" />
+	</div>
+</template>
