233e0fa5a5c5dcf1f88a2501f9cf6c0a3f6dcef7
Yonatan Kof <kof@yonatankof.com>
2024-10-01 18:47:57 +0300

    Revert "feat: Add main-warp div for improved design"
    This reverts commit ebf198754ce45247037f9aacbee7c182ccce7e7f.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,23 +3,18 @@ import { ModalsContainer } from 'vue-final-modal';
 </script>
 <template>
 	<main class="main-app">
-		<div class="main-warp">
-			<AppHeader />
-			<NuxtPage />
-			<ModalsContainer />
-			<AppFooter />
-		</div>
+		<AppHeader />
+		<NuxtPage />
+		<ModalsContainer />
+		<AppFooter />
 	</main>
 </template>
 <style lang="scss">
+
 .main-app {
-	min-height: 100vh;
-	background: radial-gradient(ellipse at bottom right, var(--color-brand-dim), transparent 50%);
-}
-.main-warp {
-	min-height: 100dvh;
+	height: 100dvh;
 	display: grid;
-	@include display-width;
 	grid-template-rows: auto 1fr auto;
+	@include display-width;
 }
 </style>
