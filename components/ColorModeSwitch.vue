86c8f34d6ff80b913eada9a29d763b0aa66cc769
Yonatan Kof <kof@yonatankof.com>
2024-10-03 08:47:39 +0300

    feat: Add ColorModeSwitch component for toggling color mode

---
+++ components/ColorModeSwitch.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+const onClick = () =>
+	colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light');
+</script>
+
+<template>
+	<button aria-label="Color Mode" @click="onClick">
+		<ColorScheme placeholder="...">
+			<template v-if="colorMode.value === 'dark'">
+				<SvgIconBase stroke-color="var(--color-brand-main)">
+					<IconDark />
+				</SvgIconBase>
+			</template>
+			<template v-else>
+				<SvgIconBase stroke-color="var(--color-brand-main)">
+					<IconLight />
+				</SvgIconBase>
+			</template>
+		</ColorScheme>
+	</button>
+</template>
