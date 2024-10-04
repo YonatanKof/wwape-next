10cc2d63e2c6ee3f3d43aa50e3e406bd2eb2af6e
Yonatan Kof <kof@yonatankof.com>
2024-10-04 18:28:10 +0300

    improve: Update intro text in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@ nuxtApp.$pageMetaTags();
 		<IntroText
 			:is-on="true"
 			title="World Wide Ape"
-			text="I’m <strong>Yonatan Ben Knaan</strong>, a graphic designer, friend, lover, family man, upright simian, an alright dude, and a mismatched persona, from <i>Tel Aviv</i>, the cultural capital of the flaming <i>Middle East</i>."
-			text2="If life is an <i>RPG</i> and you’re an <i>NPC</i>, then I’m a multiclass; professional <strong>full-stack product designer</strong> and an amateur illustrator, developer, photographer, tinkerer, and bedroom DJ."
+			text="I’m <strong>Yonatan Ben Knaan</strong>, a graphic designer, friend, lover, family man, upright simian, an alright dude, and a mismatched persona from <i>Tel Aviv</i>, the cultural capital of the flaming <i>Middle East</i>."
+			text2="If life is an <i>RPG</i> and you’re an <i>NPC</i>, then I’m a multiclass: professional <strong>full-stack product designer</strong> and an amateur illustrator, developer, photographer, tinkerer, and bedroom DJ."
 		/>
 	</main>
 </template>
