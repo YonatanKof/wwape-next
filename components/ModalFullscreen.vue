c4e8aa9bf5dfe1c8ab217dbf798ff5fac310aa27
Yonatan Kof <kof@yonatankof.com>
2024-10-05 18:27:09 +0300

    feat: Add ModalFullScreen component for displaying content in full screen mode

---
+++ components/ModalFullscreen.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal';
+
+const emit = defineEmits<{
+	(e: 'close'): void;
+}>();
+</script>
+
+<template>
+	<VueFinalModal style="display: flex; justify-content: center; align-items: center;" content-class="fullscreen-modal" content-transition="vfm-fade">
+		<button class="fullscreen-modal-btn" @click="emit('close')">close</button>
+		<slot @click="emit('close')" />
+	</VueFinalModal>
+</template>
+<style>
+.fullscreen-modal {
+	position: relative;
+	max-width: calc(100vw - var(--space-l));
+	max-height: calc(100vh - var(--space-l));
+	overflow-y: scroll;
+	border-radius: var(--border-radius-sm);
+}
+.fullscreen-modal-btn {
+	position: fixed;
+	inset-inline-end: var(--space-l);
+	inset-block-end: var(--space-l);
+}
+</style>
