ff0eeb2e3699d038bdd70d368ddc8c0411dfdfd4
Yonatan Kof <kof@yonatankof.com>
2024-10-06 08:49:49 +0300

    improve: Update FullScreenModal component styles for responsiveness

--- components/FullScreenModal.vue
+++ components/FullScreenModal.vue
@@ -7,26 +7,41 @@ const emit = defineEmits<{
 </script>
 
 <template>
-	<VueFinalModal style="display: flex; justify-content: center; align-items: center;" content-class="fullscreen-modal" content-transition="vfm-fade">
-		<button class="fullscreen-modal-btn" @click="emit('close')">close</button>
-		<slot @click="emit('close')" />
+	<VueFinalModal
+		style="display: flex; justify-content: center; align-items: center"
+		content-class="fullscreen-modal"
+		content-transition="vfm-fade"
+	>
+		<button class="fullscreen-modal-btn" @click="emit('close')">Close</button>
+		<slot />
 	</VueFinalModal>
 </template>
 <style lang="scss">
 .fullscreen-modal {
-	position: relative;
 	max-width: calc(100dvw - var(--space-l));
 	max-height: calc(100dvh - var(--space-l));
 	overflow-y: scroll;
 	border-radius: var(--border-radius-sm);
-	@media (width <= $display-width-xs) {
+	@media (width <= $display-width-sm) {
 		max-width: calc(100dvw - var(--space-m));
 		max-height: calc(100dvh - var(--space-m));
+	}
+	@media (width <= $display-width-xs) {
+		max-width: calc(100dvw - var(--space-s));
+		max-height: calc(100dvh - var(--space-s));
 	}
 }
 .fullscreen-modal-btn {
 	position: fixed;
 	inset-inline-end: var(--space-l);
 	inset-block-end: var(--space-l);
+	@media (width <= $display-width-sm) {
+		inset-inline-end: var(--space-m);
+		inset-block-end: var(--space-m);
+	}
+	@media (width <= $display-width-xs) {
+		inset-inline-end: var(--space-s);
+		inset-block-end: var(--space-s);
+	}
 }
 </style>
