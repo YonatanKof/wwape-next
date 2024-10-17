acbc5ae939a793d3e47431130d1a96b3f0e21805
Yonatan Kof <kof@yonatankof.com>
2024-10-17 20:20:42 +0300

    feat: Add vue-final-modal and FullScreenModal component for image modal functionality

--- components/content/TiltImage.vue
+++ components/content/TiltImage.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ModalsContainer, useModal } from 'vue-final-modal';
+import Modal from '~/components/FullScreenModal.vue';
+
 const props = defineProps({
 	img1: {
 		type: String,
@@ -16,16 +19,32 @@ const props = defineProps({
 		type: String,
 		default: undefined,
 	},
+	alt: {
+		type: String,
+		default: undefined,
+	},
+});
+const { open, close } = useModal({
+	component: Modal,
+	attrs: {
+		onClose() {
+			close();
+		},
+	},
+	slots: {
+		default: `<img src="${props.img2}" alt="${props.alt}" />`,
+	},
 });
 </script>
 <template>
+	<ModalsContainer />
 	<span>
-		<div>
+		<div @click="open" @keyup.enter="open">
 			<NuxtImg vi-if="img1" class="img1" :src="img1" />
 			<NuxtImg vi-if="img2" class="img2" :src="img2" />
 			<NuxtImg vi-if="img3" class="img3" :src="img3" />
 		</div>
-		<em v-if="desc" >{{ desc }}</em>
+		<em v-if="desc" v-html="desc"></em>
 	</span>
 </template>
 <style lang="scss" scoped>
@@ -41,6 +60,7 @@ span {
 	padding: var(--space-xs);
 }
 div {
+	cursor: pointer;
 	display: grid;
 	grid-template-columns: repeat(12, 1fr);
 	grid-template-rows: repeat(7, 1fr);
@@ -64,14 +84,14 @@ img {
 	grid-row: 1 / span 4;
 	transform: translateZ(-1rem);
 }
-.img2 {
+.img3 {
 	transition-delay: 125ms;
 	grid-column: 6 / span 7;
 	grid-row: 2 / span 4;
 	z-index: 1;
 	transform: translateZ(2rem);
 }
-.img3 {
+.img2 {
 	transition-delay: 250ms;
 	grid-column: 3 / span 7;
 	grid-row: 4 / span 4;
