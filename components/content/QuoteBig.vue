7d5b63b309673258b94eabdfdab4074009d63eca
Yonatan Kof <kof@yonatankof.com>
2024-10-23 13:32:26 +0300

    improve: Update QuoteBig.vue with conditional rendering of icons based on the value of the 'icon' prop

--- components/content/QuoteBig.vue
+++ components/content/QuoteBig.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 defineProps({
 	title: {
 		type: String,
@@ -8,17 +8,33 @@ defineProps({
 		type: String,
 		default: undefined,
 	},
+	icon: {
+		type: String,
+		default: 'know',
+		validator: (value: string) => {
+			return ['know', 'think'].includes(value);
+		},
+	},
 });
 </script>
 
 <template>
 	<blockquote class="quote-big">
-		<h2 class="fancy-header" unwrap="p" v-html="title"></h2>
-		<p v-if="desc" unwrap="p" v-html="desc"></p>
+		<IconBolt v-if="icon === 'know'" class="icon" />
+		<IconLightBulb v-if="icon === 'think'" class="icon" />
+		<span>
+			<h2 class="fancy-header" unwrap="p" v-html="title"></h2>
+			<p v-if="desc" unwrap="p" v-html="desc"></p>
+		</span>
 	</blockquote>
 </template>
 <style lang="scss">
 .quote-big {
+	.icon {
+		width: var(--space-2xl);
+		height: var(--space-2xl);
+		margin-inline-start: var(--space-2xs);
+	}
 	p,
 	.fancy-header {
 		margin-inline-start: var(--space-m);
@@ -32,22 +48,25 @@ defineProps({
 		text-wrap: balance;
 		font-variation-settings: 'wght' 400;
 	}
+	display: flex;
+	align-items: center;
+	flex-direction: row;
 	background-color: var(--color-brand-dis);
 	margin-block: var(--space-xs);
-	display: block;
+	// display: block;
 	padding: var(--space-s);
 	padding-inline-start: var(--space-xs);
 	border-radius: var(--border-radius-sm);
 	position: relative;
 	&::before {
-		content: '';
+		content: unset;
 		display: inline-block;
-		width: calc(var(--space-2xs) * .5);
+		width: calc(var(--space-2xs) * 0.5);
 		height: calc(100% - calc(var(--space-s) * 2));
 		background-color: var(--color-brand-main);
 		position: absolute;
 		inset-inline-start: var(--space-s);
-		border-radius: calc(var(--space-2xs) * .5);
+		border-radius: calc(var(--space-2xs) * 0.5);
 	}
 }
 </style>
