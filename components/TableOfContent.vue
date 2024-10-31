07b78006d00dfdf4d2e2d5786ecf3aaf00bc4ff8
Yonatan Kof <kof@yonatankof.com>
2024-10-31 18:22:09 +0200

    improve: Update TableOfContent component design

--- components/TableOfContent.vue
+++ components/TableOfContent.vue
@@ -14,8 +14,14 @@ const emit = defineEmits<{
 </script>
 
 <template>
-	<VueFinalModal :hide-overlay="true" :esc-to-close="false" :click-to-close="false" :lock-scroll="false" background="interactive">
-		<div>
+	<VueFinalModal
+		:hide-overlay="true"
+		:esc-to-close="false"
+		:click-to-close="false"
+		:lock-scroll="false"
+		background="interactive"
+	>
+		<div class="toc-block">
 			<span>
 				<ul v-if="tocData && tocData.links">
 					<li v-for="link in tocData.links" :key="link.text">
@@ -31,14 +37,16 @@ const emit = defineEmits<{
 						</ul>
 					</li>
 				</ul>
-				<button @click="emit('confirm')">Hide table of content</button>
 			</span>
+			<div class="hide-toc">
+				<button @click="emit('confirm')">Hide table of content</button>
+			</div>
 		</div>
 	</VueFinalModal>
 </template>
 
 <style lang="scss" scoped>
-div {
+.toc-block {
 	--base-space: var(--space-2xs);
 	position: fixed;
 	background-color: var(--color-sys-invert-highlight);
@@ -53,8 +61,8 @@ div {
 	overflow: scroll;
 }
 span {
-    display: block;
-    padding-inline: var(--base-space);
+	display: block;
+	padding-inline: var(--base-space);
 	position: relative;
 }
 a {
@@ -66,6 +74,11 @@ a {
 }
 ul {
 	padding-inline: var(--base-space);
+	// margin-block-start: var(--base-space) ;
+	z-index: 97;
+}
+span > ul > li:last-of-type {
+	margin-block-end: var(--space-s);
 }
 li {
 	line-height: 1.1;
@@ -75,9 +88,16 @@ li::marker {
 	content: unset !important;
 }
 button {
-	width: 100%;
+	position: absolute;
+	width: calc(100% - calc(var(--base-space) * 4));
+	margin-inline: calc(var(--base-space) * 2);
+}
+.hide-toc {
 	position: sticky;
-    inset-inline: var(--base-space);
-    inset-block-end: var(--base-space);
+	inset-inline: 0;
+	inset-block-end: 0;
+	height: 3.25rem;
+	background-color: var(--color-sys-invert-highlight);
+	z-index: 98;
 }
 </style>
