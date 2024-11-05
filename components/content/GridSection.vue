9e9985940aa9516629002ddb4e2bb663d8edab3d
Yonatan Kof <kof@yonatankof.com>
2024-11-05 16:38:48 +0200

    improve: Add GridSection component with customizable width and margin properties

---
+++ components/content/GridSection.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+	sectionWidth: {
+		type: String,
+		default: 'var(--display-width-md)',
+	},
+	marginBlockStart: {
+		type: String,
+		default: undefined,
+	},
+});
+</script>
+
+<template>
+	<div class="grid-section">
+		<slot />
+	</div>
+</template>
+<style lang="scss">
+.grid-section {
+	@include display-width(v-bind(sectionWidth));
+	// @media (width <= $display-width-sm) {
+	// 	margin-block-start: unset;
+	// }
+	// & > :first-child {
+	// 	margin-block-start: unset;
+	// }
+}
+</style>
