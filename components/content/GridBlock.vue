9617d7554c2946cb4782c0d942e8ae1580832486
Yonatan Kof <kof@yonatankof.com>
2024-11-05 16:39:38 +0200

    improve: Refactor GridBlock component to support customizable grid templates and enhance accessibility with dynamic href generation

--- components/content/GridBlock.vue
+++ components/content/GridBlock.vue
@@ -1,35 +1,95 @@
-<script setup>
+<script setup lang="ts">
 defineProps({
-	colNum: {
+	colTemplate: {
 		type: String,
-		default: "2",
+		default: '1fr 1fr',
 	},
-	rowNum: {
+	rowTemplate: {
 		type: String,
-		default: "1",
+		default: '1fr',
 	},
 	gap: {
 		type: String,
-		default: 'var(--space-s)',
+		default: 'var(--space-l)',
+	},
+	maxWidth: {
+		type: String,
+		default: '100%',
+	},
+	title: {
+		type: String,
+		default: undefined,
+	},
+	desc: {
+		type: String,
+		default: undefined,
 	},
 });
+
+/**
+ * Converts a string to a URL-friendly href format
+ * @param text Input string to convert
+ * @returns Formatted href string
+ */
+const generateHref = (text: string): string => {
+	return (
+		text
+			.toLowerCase() // Convert to lowercase
+			.replace(/[^a-z0-9\s-]/g, '') // Remove special characters except spaces and hyphens
+			.trim() // Remove leading/trailing spaces
+			.replace(/\s+/g, '-')
+	); // Replace spaces with hyphens
+};
+
+// Example usage:
+// const title = 'The Dynamic Duo: Fixed + Changing Panel';
+// const href = generateHref(title);
+// Output: '#the-dynamic-duo-fixed-changing-panel'
+
 </script>
 <template>
-	<div>
-        <slot />
-    </div>
+	<section>
+		<span v-if="title">
+			<h3 :id="generateHref(title)">
+				<a style="width: 100%" :href="'#' + generateHref(title)"> {{ title }} </a>
+			</h3>
+			<p id="sub-title" v-if="desc">{{ desc }}</p>
+			<hr />
+		</span>
+		<div>
+			<slot />
+		</div>
+	</section>
 </template>
 <style lang="scss" scoped>
+section {
+	background-color: var(--color-sys-invert-highlight);
+	height: 100%;
+	padding-block: var(--space-l);
+	margin-block: var(--space-l);
+}
 div {
 	display: grid;
-	grid-template-columns: repeat(v-bind(colNum), 1fr);
-	grid-template-rows: repeat(v-bind(rowNum), 1fr);
+	grid-template-columns: v-bind(colTemplate);
+	grid-template-rows: v-bind(rowTemplate);
 	gap: v-bind(gap);
-	height: 100%;
-	width: 100%;
-	margin-block: var(--space-xs);
-	@media (width <= $display-width-xs) {
+	@include display-width($width: v-bind(maxWidth));
+	@media (width <= $display-width-sm) {
 		grid-template-columns: 1fr;
 	}
+}
+span {
+	display: block;
+	@include display-width($width: v-bind(maxWidth));
+	margin-block-end: var(--space-l);
+}
+.title,
+#sub-title {
+	margin-block-start: unset;
+	margin-block-end: var(--space-m);
+}
+
+:is(.title):has(+ p) {
+	margin-block-end: var(--space-3xs);
 }
 </style>
