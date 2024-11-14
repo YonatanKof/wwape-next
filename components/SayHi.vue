8493902f265a8d1ef2a477ec8618a48c6cdf6f39
Yonatan Kof <kof@yonatankof.com>
2024-11-14 14:45:37 +0200

    improve: Add more hellos to  SayHi

--- components/SayHi.vue
+++ components/SayHi.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import VueTypewriterEffect from "vue-typewriter-effect";
+<script setup>
+import VueTypewriterEffect from 'vue-typewriter-effect';
 </script>
 
 <template>
 	<h1>
-		<vue-typewriter-effect :strings="['Hello', 'שלום', 'Привіт', 'مرحبًا', 'Hola', 'こんにちは', 'Hey', 'Bonjour', 'Salut', '你好', 'Привет']" />  
+		<vue-typewriter-effect
+			:strings="[
+				'Hello',
+				'שלום',
+				'Привіт',
+				'مرحبًا',
+				'Γειά σου',
+				'Hola',
+				'こんにちは',
+				'Hey',
+				'안녕하세요',
+				'Bonjour',
+				'Salut',
+				'你好',
+				'Привет',
+				'Shwari',
+			]"
+		/>
 	</h1>
-</template>
+</template> 
