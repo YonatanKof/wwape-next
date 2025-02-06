37ae662fe1a1293cf5ea4c3fea78902498f8673e
Yonatan Kof <kof@yonatankof.com>
2025-02-06 21:42:18 +0200

    improve: Reorder navigation links and adjust responsive styles

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -16,9 +16,9 @@ defineProps({
 				<nuxt-link :to="{ name: 'index' }">Visit WWape</nuxt-link>
 			</nav>
 			<nav v-show="!isSimple">
-				<nuxt-link :to="{ name: 'work' }">Work</nuxt-link>
 				<nuxt-link :to="{ name: 'design' }" aria-label="Go to Graphic Arts & Design page">Graphics</nuxt-link>
 				<nuxt-link :to="{ name: 'post' }" aria-label="Go to Post page">Posts</nuxt-link>
+				<nuxt-link :to="{ name: 'work' }">Work</nuxt-link>
 			</nav>
 			<ColorModeSwitch />
 		</div>
@@ -28,37 +28,45 @@ defineProps({
 .home-link {
 	border: none;
 }
+
 .kof-logo {
 	width: var(--space-2xl);
 	height: var(--space-2xl);
 	margin-inline-end: var(--space-2xs);
 	transition: filter 0.35s ease-in-out, transform 0.25s ease-in-out;
 	filter: drop-shadow(0.075em 0.075em 0.2em var(--color-black-dim));
+
 	&:hover {
 		transform: scale(1.05) rotate(0.01turn) translate(0.1em, 0.1em);
 		filter: drop-shadow(0.05em 0.05em 0.25em var(--color-black-dim));
 	}
 }
+
 i {
 	@include flex-center();
 }
+
 header {
 	height: var(--header-height);
 	@include flex-center;
 }
+
 #last-name {
 	white-space: nowrap;
 }
+
 #header-start {
 	display: flex;
 	align-items: center;
 }
+
 div,
 nav {
 	@include flex-center;
 	gap: var(--space-xs);
 }
-@media (width <= $display-width-2xs) {
+
+@media (width <=$display-width-2xs) {
 	header {
 		flex-direction: column-reverse;
 		align-items: start;
@@ -66,10 +74,12 @@ nav {
 		padding-block: var(--space-xs);
 		height: unset;
 	}
+
 	div {
 		width: 100%;
 	}
 }
+
 @media print {
 	header {
 		display: none !important;
