de56c302129bb3648c51a2cdfcfd431ac9b56096
Yonatan Kof <kof@yonatankof.com>
2025-04-05 20:23:57 +0300

    Show comp in index

--- pages/index.vue
+++ pages/index.vue
@@ -10,5 +10,11 @@ nuxtApp.$pageMetaTags();
 			text="I’m <strong>Yonatan Ben Knaan</strong>, a graphic designer, friend, lover, family man, upright simian, an alright dude, and a mismatched persona from <i>Tel Aviv</i>, the cultural capital of the flaming <i>Middle East</i>."
 			text2="If life is an <i>RPG</i> and you’re an <i>NPC</i>, then I’m a multiclass: professional <strong>full-stack product designer</strong> and an amateur illustrator, developer, photographer, tinkerer, and bedroom DJ."
 		/>
+		<div>
+			<h2>Theme Settings</h2>
+			<ColorPicker cssVariable="--color-sys-main" label="Main System Color" />
+			<ColorPicker cssVariable="--color-sys-invert-main" label="Inverted Main Color" />
+			<ColorPicker cssVariable="--color-brand-main" label="Brand Color" />
+		</div>
 	</main>
 </template>
