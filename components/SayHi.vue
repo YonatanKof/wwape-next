3c6821b6a85f02cec902b32b6cb86b0664826502
Yonatan Kof <kof@yonatankof.com>
2025-05-03 11:19:19 +0300

    improve: Update greeting strings in SayHi.vue for better multilingual support

--- components/SayHi.vue
+++ components/SayHi.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
+<script setup>
 import VueTypewriterEffect from "vue-typewriter-effect";
 </script>
 
 <template>
 	<span>
-		<vue-typewriter-effect :strings="['Hello', 'שלום', 'Привіт', 'مرحبًا', 'Hola', 'こんにちは', 'Hey', 'Bonjour', 'Salut', '你好', 'Привет']" />  👋 
+		<vue-typewriter-effect :strings="['Hello', 'नमस्ते', '你好', 'Hola', 'Bonjour', 'مرحبًا', 'হ্যালো', 'Привет', 'Olá', 'Halo', 'ہیلو', 'Hallo', 'こんにちは', 'Xin chào', 'Merhaba', '你好', 'שלום']" />  👋🏻
 	</span>
 </template>
