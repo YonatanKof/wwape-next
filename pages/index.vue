66654b35f4ea0f8c990ca07617808795c7117bfd
Yonatan Kof <kof@yonatankof.com>
2025-08-02 19:15:41 +0300

    content: Improve homepage text

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@ nuxtApp.$pageMetaTags();
 		<IntroText
 			:is-on="true"
 			title="World Wide Ape"
-			text="I’m <i>Yonatan Ben Knaan</i>, a mismatched persona and an alright dude from <i>Tel Aviv</i>, the cultural capital of the flaming <i>middle east</i>."
+			text="I’m <i>Yonatan Ben Knaan</i>, a family man, graphic designer, friend, and an alright dude from <i>Tel Aviv</i>, the cultural capital of the flaming <i>middle east</i>."
 			text2="If life is an <i>RPG</i> and you’re an <i>NPC</i>, then I’m a multiclass product designer and amateur illustrator, developer, photographer, and bedroom DJ."
 		/>
 	</main>
