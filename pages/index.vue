c40f125d9ecb551dcb9d78e087c241b8819684e8
Yonatan Kof <kof@yonatankof.com>
2025-08-23 18:51:22 +0300

    improve: Styles and content in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,12 @@ const nuxtApp = useNuxtApp();
 nuxtApp.$pageMetaTags();
 
 const { data: design } = await useAsyncData('design-date', () => {
-	return queryContent('design').limit(5).only(['title', 'cover_image', 'image_alt']).sort({ date: -1 }).where({}).find();
+	return queryContent('design')
+		.limit(6)
+		.only(['title', 'social_image', 'image_alt'])
+		.sort({ date: -1 })
+		.where({})
+		.find();
 });
 const { data: post } = await useAsyncData('post-date', () => {
 	return queryContent('post').limit(3).only(['title', 'description']).sort({ date: -1 }).where({}).find();
@@ -15,6 +20,31 @@ const { data: post } = await useAsyncData('post-date', () => {
 			:is-on="true"
 			title="World Wide Ape"
 			text="I’m <i>Yonatan Ben Knaan</i>, a graphic designer and an alright dude from <i>Tel Aviv</i>, the cultural capital of the flaming <i>middle east</i>."
+		/>
+		<section class="bento">
+			<ShowContentPreview
+				style="grid-column: 1 / 4; grid-row: 1 / -1"
+				:content-data="design"
+				title="Latest design works"
+				link-to="design"
 			/>
+			<span style="grid-column: 4 / -1"
+				><ShowContentPreview :content-data="post" title="Latest articles" link-to="post" isLink />
+				<ShowContentPreview :content-data="post" title="Latest articles" link-to="post" isLink
+			/></span>
+		</section>
 	</main>
 </template>
+
+<style scoped>
+.bento {
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+	gap: var(--space-s);
+	span {
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-s);
+	}
+}
+</style>
