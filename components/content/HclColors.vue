2eb4cd344269805cf52589a88a3ac36f0f3c9b05
Yonatan Ben Knaan <kof@yonatankof.com>
2025-09-09 09:52:28 +0300

    improve: Now baseHue input controls the the hue

--- components/content/HclColors.vue
+++ components/content/HclColors.vue
@@ -2,21 +2,28 @@
 defineProps({
 	color: {
 		type: String,
-		default: 'hwb(120 0% 0%)',
+		default: 'hwb(0 0% 0%)',
+	},
+	hue: {
+		type: Number,
+		default: 0,
 	},
 });
+const baseHue = ref(0);
 </script>
 
 <template>
 	<section>
 		<h1>The hwb() with from</h1>
+		<input type="range" id="hue" name="hue" min="0" max="359" v-model="baseHue" />
+		<label for="hue">{{ baseHue }}</label>
 		<span>
-			<div v-for="i in 11" :style="`background-color: hwb(from ` + color + ` h calc(w + ` + (i - 1) * 10 + `) b);`">
+			<div v-for="i in 11" :style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue +`) calc(w + ` + (i - 1) * 10 + `) b);`">
 				<h6>{{ (i - 1) * 10 }}</h6>
 			</div>
 		</span>
 		<span>
-			<div v-for="i in 11" :style="`background-color: hwb(from ` + color + ` h w calc(b + ` + (i - 1) * 10 + `) );`">
+			<div v-for="i in 11" :style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue +`) w calc(b + ` + (i - 1) * 10 + `) );`">
 				<h6 style="filter: invert(1)">{{ (i - 1) * 10 }}</h6>
 			</div>
 		</span>
---
+++ content/post/hcl-colors.md
@@ -0,0 +1,35 @@
+---
+title: 'HCL Colors'
+author_name: 'Yonatan Ben Knaan'
+date: '2025-09-08'
+updated: ''
+cover_image: '/posts/'
+cover_image_height: ''
+cover_image_thumbhash: ''
+social_image: '/posts/'
+image_alt: ''
+description: ''
+tags: ['Creative']
+---
+
+<!-- {{ $doc.description }} -->
+
+<!-- ::lazy-img{src="/posts/..." thumbhash="" alt="" desc=""}
+:: -->
+
+<!-- ![...](/posts/.../...) -->
+
+The hwb() functional notation expresses a color in the sRGB color space according to its hue, whiteness, and blackness.
+An optional alpha component represents the color's transparency.
+
+::HclColors
+::
+
+<!-- ::HclColors{color="red"} 
+:: -->
+
+<!-- ::YouTubePlayer{videoId="" title=""}
+:: -->
+
+<!-- ![image](md-content-types/Yonatan_Kof.png)
+*Now that's one good looking Kof* -->
