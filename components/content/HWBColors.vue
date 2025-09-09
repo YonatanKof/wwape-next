7f671b542d45ab9a3e92fbe25d4a8236a1695cc3
Yonatan Ben Knaan <kof@yonatankof.com>
2025-09-09 11:47:10 +0300

    improve: Rename file and add image

--- components/content/HWBColors.vue
+++ components/content/HWBColors.vue
@@ -18,24 +18,28 @@ const baseHue = ref(Math.floor(Math.random() * 360));
 		<h4>Color pallet for base hue to Black and White</h4>
 		<label for="hue"
 			>Update hue
-			<code>{{ Number(baseHue) + 1 }}</code> 
-      <i> (it's acutely 0 to 359 because of zero-based numbering)</i>
+			<code>{{ Number(baseHue) + 1 }}</code>
+			<i> (it's acutely 0 to 359 because of zero-based numbering)</i>
 		</label>
 		<input type="range" id="hue" name="hue" min="0" max="359" v-model="baseHue" />
 		<span>
+			<span id="white-shift">
+				<div 
+					v-for="i in 11"
+					:style="
+						`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) calc(w + ` + (i - 1) * 10 + `) b);`
+					"
+				>
+					<p>{{ (i - 1) * 10 }}</p>
+				</div>
+			</span id="black-shift">
 			<div 
 				v-for="i in 11"
-				:style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) w calc(b + ` + (i - 1) * 10 + `) );`"
-				>
+				:style="
+					`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) w calc(b + ` + (i - 1) * 10 + `) );`
+				"
+			>
 				<p style="filter: invert(1)">{{ (i - 1) * 10 }}</p>
-			</div>
-		</span>
-		<span>
-			<div
-				v-for="i in 11"
-				:style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) calc(w + ` + (i - 1) * 10 + `) b);`"
-				>
-				<p>{{ (i - 1) * 10 }}</p>
 			</div>
 		</span>
 	</section>
