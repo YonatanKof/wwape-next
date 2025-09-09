0cc0772a16b9b41a98595b2b5c1d3f8f0ed8f25f
Yonatan Ben Knaan <kof@yonatankof.com>
2025-09-09 10:51:06 +0300

    Improve: Set random and better styles

--- components/content/HclColors.vue
+++ components/content/HclColors.vue
@@ -9,22 +9,33 @@ defineProps({
 		default: 0,
 	},
 });
-const baseHue = ref(0);
+const baseHue = ref(Math.floor(Math.random() * 360));
 </script>
 
 <template>
 	<section>
 		<h1>The hwb() with from</h1>
+		<h4>Color pallet for base hue to Black and White</h4>
+		<label for="hue"
+			>Update hue
+			<code>{{ Number(baseHue) + 1 }}</code> 
+      <i> (it's acutely 0 to 359 because of zero-based numbering)</i>
+		</label>
 		<input type="range" id="hue" name="hue" min="0" max="359" v-model="baseHue" />
-		<label for="hue">{{ baseHue }}</label>
 		<span>
-			<div v-for="i in 11" :style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue +`) calc(w + ` + (i - 1) * 10 + `) b);`">
-				<h6>{{ (i - 1) * 10 }}</h6>
+			<div 
+				v-for="i in 11"
+				:style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) w calc(b + ` + (i - 1) * 10 + `) );`"
+				>
+				<p style="filter: invert(1)">{{ (i - 1) * 10 }}</p>
 			</div>
 		</span>
 		<span>
-			<div v-for="i in 11" :style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue +`) w calc(b + ` + (i - 1) * 10 + `) );`">
-				<h6 style="filter: invert(1)">{{ (i - 1) * 10 }}</h6>
+			<div
+				v-for="i in 11"
+				:style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) calc(w + ` + (i - 1) * 10 + `) b);`"
+				>
+				<p>{{ (i - 1) * 10 }}</p>
 			</div>
 		</span>
 	</section>
@@ -37,18 +48,24 @@ section {
 	gap: 0.25rem;
 }
 span {
-	display: flex;
-	gap: 0.25rem;
+	display: grid;
+	grid-template-columns: repeat(11, 1fr);
+	gap: var(--space-3xs);
 }
 div {
-	width: 3rem;
 	aspect-ratio: 1/1;
+	border-radius: var(--space-3xs);
 	background-color: v-bind(color);
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	& * {
-		margin-block-start: 0.25em;
+		margin-block-start: var(--space-3xs);
 	}
+}
+p {
+	font-size: var(--step--2);
+	font-family: var(--font-hand);
+	font-weight: 900;
 }
 </style>
