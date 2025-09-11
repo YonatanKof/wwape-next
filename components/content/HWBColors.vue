daa437b661a531629eb2e6d4333031f8c4c715a8
Yonatan Ben Knaan <kof@yonatankof.com>
2025-09-11 12:36:58 +0300

    improve: Add w to b example and better titles

--- components/content/HWBColors.vue
+++ components/content/HWBColors.vue
@@ -14,10 +14,12 @@ const baseHue = ref(Math.floor(Math.random() * 360));
 	<section>
 		<!-- <div style="width: 4rem; height: 4rem; background-color: hwb(from var(--my-color) h w calc(b + 50));"></div> -->
 		<label for="hue"
-			>Update hue
-			<code>{{ baseHue }}</code>
+			><h4>
+				Update hue <code>{{ baseHue }}</code>
+			</h4>
 		</label>
 		<input type="range" id="hue" name="hue" min="0" max="359" v-model="baseHue" />
+		<h4>From pure huw to pure white – 10 steps</h4>
 		<div class="color-line" id="white-shift">
 			<div
 				class="color-box"
@@ -27,6 +29,7 @@ const baseHue = ref(Math.floor(Math.random() * 360));
 				<p>{{ (i - 1) * 10 }}</p>
 			</div>
 		</div>
+		<h4>From pure huw to pure black – 10 steps</h4>
 		<div class="color-line" id="black-shift">
 			<div
 				class="color-box"
@@ -36,6 +39,29 @@ const baseHue = ref(Math.floor(Math.random() * 360));
 				"
 			>
 				<p style="filter: invert(1)">{{ (i - 1) * 10 }}</p>
+			</div>
+		</div>
+		<h4>From pure white to pure huw to pure black – 10 steps</h4>
+		<div class="color-line" id="both">
+			<div class="color-line-flipped">
+				<div
+					class="color-box"
+					v-for="i in 5"
+					:style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) calc(w + ` + i * 20 + `) b);`"
+				>
+					<p>{{ i * 20 }}</p>
+				</div>
+			</div>
+
+			<div class="color-box" :style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) w b);`">
+				<p style="filter: invert(1)">0</p>
+			</div>
+			<div
+				class="color-box"
+				v-for="i in 5"
+				:style="`background-color: hwb(from ` + color + ` calc(h + ` + baseHue + `) w calc(b + ` + i * 20 + `) );`"
+			>
+				<p style="filter: invert(1)">{{ i * 20 }}</p>
 			</div>
 		</div>
 	</section>
@@ -53,6 +79,12 @@ section {
 	grid-template-columns: repeat(11, 1fr);
 	gap: var(--space-3xs);
 }
+.color-line-flipped {
+	grid-column: 1 / 6;
+	grid-template-columns: subgrid;
+	display: grid;
+	direction: rtl;
+}
 .color-box {
 	aspect-ratio: 1;
 	border-radius: var(--space-3xs);
@@ -64,9 +96,15 @@ section {
 		margin-block-start: var(--space-3xs);
 	}
 }
+
 p {
 	font-size: var(--step--2);
 	font-family: var(--font-hand);
 	font-weight: 900;
+	text-shadow: 0.5px 0.5px 0.5px var(--color-sys-invert-slight);
+}
+h4 {
+	margin-block-start: 0.75em;
+	margin-block-end: 0.5em;
 }
 </style>
