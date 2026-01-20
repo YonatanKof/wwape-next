54803262d48ac46d56cb6fa9551ab025ac84357d
Yonatan Kof <kof@yonatankof.com>
2026-01-20 13:23:57 +0200

    Remove carousel HTML and use Carousel component + design improvements

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,21 @@ const { data: design } = await useAsyncData('design-date', () => {
 });
 
 const { data: post } = await useAsyncData('post-date', () => {
-	return queryContent('post').limit(4).only(['title', 'description']).sort({ date: -1 }).where({}).find();
+	return queryContent('post')
+		.limit(4)
+		.only(['title', 'cover_image', 'cover_image_thumbhash', 'cover_image_height', 'image_alt'])
+		.sort({ date: -1 })
+		.where({})
+		.find();
 });
-import 'vue3-carousel/carousel.css';
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
-
-const carouselConfig = {
-	itemsToShow: 1,
-	wrapAround: true,
-	autoplay: 2500,
-};
+const { data: work } = await useAsyncData('work-date', () => {
+	return queryContent('work')
+		.limit(2)
+		.only(['title', 'cover_image', 'cover_image_thumbhash', 'cover_image_height', 'image_alt'])
+		.sort({ date: -1 })
+		.where({})
+		.find();
+});
 </script>
 <template>
 	<main>
@@ -31,30 +36,9 @@ const carouselConfig = {
 			text="I’m <i>Yonatan Ben Knaan</i>, a graphic designer and an alright dude from <i>Tel Aviv</i>, the cultural capital of the flaming <i>middle east</i>."
 		/>
 		<section class="bento">
-			<Carousel class="item" v-bind="carouselConfig">
-				<Slide v-for="item in design" :key="item">
-					<UnLazyImage
-						class="carousel-image"
-						:thumbhash="item.cover_image_thumbhash"
-						:src="item.cover_image"
-						:alt="item.image_alt"
-						width="1000"
-						:height="item.cover_image_height"
-					/>
-				</Slide>
-				<template #addons>
-					<Pagination />
-				</template>
-			</Carousel>
-
-			<!-- <ShowContentPreview class="block-design" :content-data="design" sec-title="Latest graphic art" link-to="design" />
-			<ShowContentPreview class="block-post" :content-data="post" sec-title="Latest posts" link-to="post" isLink />
-			<div class="block-music">
-				<span>
-					<p>Playlists</p>
-					<p>and Mixes</p>
-				</span>
-			</div> -->
+			<Carousel isLink :link-to="design" class="block-design" :content-data="design" :duration="2222" />
+			<Carousel isLink :link-to="post" class="block-post" :content-data="post" :duration="3333" aspect-ratio="4 / 3" />
+			<Carousel isLink :link-to="work" class="block-work" :content-data="work" :duration="4444" aspect-ratio="3 / 2" />
 		</section>
 	</main>
 </template>
@@ -62,76 +46,35 @@ const carouselConfig = {
 <style scoped>
 .bento {
 	display: grid;
-	grid-template-columns: repeat(5, 1fr);
-	grid-template-rows: 1fr auto;
-	gap: var(--space-s);
-	margin-block-end: var(--space-s);
-	span {
-		display: flex;
-		flex-direction: column;
-		gap: var(--space-s);
-	}
+	grid-template-columns: repeat(12, 1fr);
+	grid-template-rows: auto var(--space-10xl) auto;
+	padding-block: var(--space-m);
 	@media (width <= 544px) {
 		grid-template-columns: 1fr;
 	}
 }
 .block-design {
-	grid-column: 1 / 4;
-	grid-row: 1;
+	grid-column: 1 / span 5;
+	grid-row: 1 / span 2;
+	z-index: 10;
 	@media (width <= 544px) {
 		grid-column: unset;
 	}
 }
 .block-post {
-	grid-column: 4 / -1;
-	grid-row: 1 / -1;
+	grid-column: 5 / span 5;
+	grid-row: 2 / span 2;
+	z-index: 9;
 	@media (width <= 544px) {
 		grid-column: unset;
 	}
 }
-@keyframes music {
-	0% {
-		transform: rotate3d(1, 2, 3, 16deg);
-	}
-
-	50% {
-		transform: rotate3d(0, 1, 0, 32deg);
-	}
-	100% {
-		transform: rotate3d(3, 2, 1, 64deg);
-	}
-}
-.block-music {
-	grid-column: 1 / 4;
-	grid-row: 2;
-	height: fit-content;
-	background-color: var(--color-sys-invert-highlight);
-	overflow: auto;
-	border-radius: var(--border-radius-xs);
-	transition: transform 0.25s, box-shadow ease-in-out 0.25s;
-	transform: translateY(0);
-	& span {
-		perspective: 180px;
-		animation: 5s infinite alternate music;
-	}
-	& p {
-		font-family: var(--font-hand);
-		line-height: 0.75;
-		font-size: 600%;
-		text-align: center;
-		white-space: nowrap;
-	}
-	word-break: normal;
-	overflow: hidden;
-	&:hover {
-		transform: translateY(calc(var(--space-3xs) * -1));
-	}
+.block-work {
+	grid-column: 8 / -1;
+	grid-row: 1 / span 2;
+	margin-block-start: var(--space-m);
 	@media (width <= 544px) {
 		grid-column: unset;
 	}
-}
-.carousel-image{
-	aspect-ratio: 1;
-	object-fit: cover;
 }
 </style>
